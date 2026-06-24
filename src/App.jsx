1d65c27e0694f735af1690558e12b34f35624110
Shawn Seo <sseo2@Shawns-MacBook-Pro-2.local>
2026-06-24 10:03:14 -0700

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,143 +1,26 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <section id="center">
-//         <div className="hero">
-//           <img src={heroImg} className="base" width="170" height="179" alt="" />
-//           <img src={reactLogo} className="framework" alt="React logo" />
-//           <img src={viteLogo} className="vite" alt="Vite logo" />
-//         </div>
-//         <div>
-//           <h1>Get started</h1>
-//           <p>
-//             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-//           </p>
-//         </div>
-//         <button
-//           type="button"
-//           className="counter"
-//           onClick={() => setCount((count) => count + 1)}
-//         >
-//           Count is {count}
-//         </button>
-//       </section>
-
-//       <div className="ticks"></div>
-
-//       <section id="next-steps">
-//         <div id="docs">
-//           <svg className="icon" role="presentation" aria-hidden="true">
-//             <use href="/icons.svg#documentation-icon"></use>
-//           </svg>
-//           <h2>Documentation</h2>
-//           <p>Your questions, answered</p>
-//           <ul>
-//             <li>
-//               <a href="https://vite.dev/" target="_blank">
-//                 <img className="logo" src={viteLogo} alt="" />
-//                 Explore Vite
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://react.dev/" target="_blank">
-//                 <img className="button-icon" src={reactLogo} alt="" />
-//                 Learn more
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//         <div id="social">
-//           <svg className="icon" role="presentation" aria-hidden="true">
-//             <use href="/icons.svg#social-icon"></use>
-//           </svg>
-//           <h2>BYE BYE BYEB YE BYEBYEBYB</h2>
-//           <p>Join the Vite community</p>
-//           <ul>
-//             <li>
-//               <a href="https://github.com/vitejs/vite" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#github-icon"></use>
-//                 </svg>
-//                 GitHub
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://chat.vite.dev/" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#discord-icon"></use>
-//                 </svg>
-//                 Discord
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://x.com/vite_js" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#x-icon"></use>
-//                 </svg>
-//                 X.com
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://bsky.app/profile/vite.dev" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#bluesky-icon"></use>
-//                 </svg>
-//                 Bluesky
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//       </section>
-
-//       <div className="ticks"></div>
-//       <section id="spacer"></section>
-//     </>
-//   )
-// }
-
-// export default App
-
+import "./App.css";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-// import characters from './protagonists.json'
-import './App.css'
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 function App() {
   return (
     <>
-      <Container maxWidth="md" sx={{ mb: 4 }}>
+      <Container maxWidth="lg">
         <Typography
           variant="h2"
           align="center"
+          s
           color="text.primary"
           sx={{ py: 2 }}
         >
-          CSS Mystery
+          Bay Area Nature
         </Typography>
         <Typography
           variant="h5"
@@ -145,23 +28,123 @@ function App() {
           color="text.secondary"
           sx={{ mx: 10 }}
         >
-          Why isn't the CSS working?
+          Shawn Seo's
+          <Box component="span" sx={{ color: "error.main" }}>
+            {" "}
+            Gatekept
+          </Box>{" "}
+          Locations
         </Typography>
-      </Container>
 
-      <Container maxWidth="lg">
-        <Button color="error"
-          variant="contained"
-          // I'm trying to use custom CSS defined in the file App.css,
-          // but it isn't working. Why, and how can I fix it?
-          className="characterButton"
-        >
-          Red Button?
-        </Button>
+        <Grid container spacing={3} sx={{ py: 7 }}>
+          <Grid>
+            <Card sx={{ maxWidth: 350 }}>
+              <CardMedia
+                sx={{ height: 250 }}
+                image="src/assets/limantour_beach.jpeg"
+                title="Best Bonfire Beach spot"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Limantour Beach
+                </Typography>
+                <Typography>
+                  <ul>
+                    <li>
+                      <strong>Reason:</strong> Best secluded beach spot that
+                      allows bonfires, and sunset is super pretty
+                    </li>
+                    <li>
+                      <strong>Distance:</strong> 1.5 hr drive from San Francisco
+                    </li>
+                    <li>
+                      <strong>Tip:</strong> Super pretty when the sun sets, but
+                      bring flashlight since there's no streetlight{" "}
+                    </li>
+                  </ul>
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="secondary">
+                  Shawn's Rating: 9.4/10
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid>
+            <Card sx={{ maxWidth: 350 }}>
+              <CardMedia
+                sx={{ height: 250 }}
+                image="src/assets/little_yosemite.jpeg"
+                title="Little Yoesmite"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Little Yoesmite
+                </Typography>
+                <Typography>
+                  <ul>
+                    <li>
+                      <strong>Reason:</strong> Best secluded hiking spot that
+                      has a waterfall and a small lake
+                    </li>
+                    <li>
+                      <strong>Distance:</strong> 1 hr drive from San Francisco
+                    </li>
+                    <li>
+                      <strong>Tip:</strong> Bring 5 dollars in cash for parking
+                      or go in before 7am to get free parking -- but it's niche
+                      so dw ab parking space{" "}
+                    </li>
+                  </ul>
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="secondary">
+                  Shawn's Rating: 8.8/10
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid>
+            <Card sx={{ maxWidth: 350 }}>
+              <CardMedia
+                sx={{ height: 250 }}
+                image="src/assets/stinon_view_point.jpeg"
+                title="Stinson View Point"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Stinson Beach View Point
+                </Typography>
+                <Typography>
+                  <ul>
+                    <li>
+                      <strong>Reason:</strong> Beach is popular but hiking up
+                      mountain to see view point is secluded. Above clouds and
+                      you can see the ocean and mountains
+                    </li>
+                    <li>
+                      <strong>Distance:</strong> 1 hr drive from San Francisco
+                    </li>
+                    <li>
+                      <strong>Tip:</strong> Bring a jacket since it can get
+                      quite chilly at the top{" "}
+                    </li>
+                  </ul>
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="secondary">
+                  Shawn's Rating: 9.1/10
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
       </Container>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
