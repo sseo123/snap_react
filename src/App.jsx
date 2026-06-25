78e42a26d35f2d69182be060ed5022781dfb97c5
Shawn Seo <sseo2@Shawns-MacBook-Pro-2.local>
2026-06-25 14:14:44 -0700

    Added search feature

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,32 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import DataCard from "./DataCard";
 import my_data from "./locations.json";
+import TextField from "@mui/material/TextField";
+import * as React from "react";
 
 function App() {
+  const [userInput, setUserInput] = React.useState("");
+
+  const handleSubmit = () => {
+    console.log(userInput);
+  };
+
+  function handleUserInput(e) {
+    const { value } = e.target;
+    setUserInput(value);
+  }
+
+  const search_filter = my_data.filter(
+    (item) => item.location.toLowerCase().includes(userInput.toLowerCase()),
+    console.log(userInput),
+  );
+
   return (
     <>
       <Container maxWidth="lg">
         <Typography
           variant="h2"
           align="center"
-          s
           color="text.primary"
           sx={{ py: 2 }}
         >
@@ -38,16 +55,44 @@ function App() {
           Locations
         </Typography>
 
-        <Grid container spacing={3} sx={{ py: 7 }}>
-          {my_data.map((data) => (
-            <DataCard
-              img={data.img}
-              location={data.location}
-              rating={data.rating}
-              bullet_point={data.bullet_point_data}
-            />
-          ))}
-        </Grid>
+        <Button sx={{ m: 1 }} onClick={handleSubmit}>
+          Submit
+        </Button>
+
+        <Box
+          component="form"
+          sx={{ "& > :not(style)": { m: 1, width: "100ch" }, py: 3 }}
+          noValidate
+          autoComplete="off"
+          align="center"
+        >
+          <TextField
+            id="outlined-controlled"
+            label="Search for location"
+            value={userInput}
+            onChange={handleUserInput}
+          />
+        </Box>
+
+        <Box>
+          {search_filter.length > 0 ? (
+            <Grid container spacing={3} sx={{ py: 1 }}>
+              {search_filter.map((data) => (
+                <DataCard
+                  img={data.img}
+                  location={data.location}
+                  rating={data.rating}
+                  bullet_point={data.bullet_point_data}
+                />
+              ))}
+            </Grid>
+          ) : (
+            <Typography align="center" sx={{ py: 10 }} variant="h3">
+              {" "}
+              NOTHING MATCHED YOUR SEARCH
+            </Typography>
+          )}
+        </Box>
       </Container>
     </>
   );
